ee9b4b24a972c69b3bb0fc2679f6f358208b727a
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2022-11-30 20:21:41 +0300

    Adds ligatures

--- init.el
+++ init.el
@@ -10,8 +10,14 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 115)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
 
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 150)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 150 :weight 'regular)
+ 
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -43,6 +49,30 @@
                 shell-mode-hook
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+
+
+
+
+(use-package ligature
+  :config
+  (ligature-set-ligatures 't '("www"))
+  (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
+                                     ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
+                                     "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
+                                     "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
+                                     "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
+                                     "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
+                                     "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
+                                     "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
+                                     "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
+                                     "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
+
+ (global-ligature-mode 't))
+
+
+
 
 (use-package ivy
   :diminish
@@ -111,19 +141,59 @@
 
 (use-package magit)
 
+;; Org Mode Configuration ------------------------------------------------------
 
 (defun azd/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
   (visual-line-mode 1))
 
+(defun azd/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+ ;; Set faces for heading levels
+ (dolist (face '((org-level-1 . 1.2)
+                 (org-level-2 . 1.1)
+                 (org-level-3 . 1.05)
+                 (org-level-4 . 1.0)
+                 (org-level-5 . 1.1)
+                 (org-level-6 . 1.1)
+                 (org-level-7 . 1.1)
+                 (org-level-8 . 1.1)))
+   (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+ ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+ (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+ (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+ (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+ (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+ (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+ (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+ (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+  
 (use-package org
-  ;; :hook (org-mode . azd/org-mode-setup)
+  :hook (org-mode . azd/org-mode-setup)
   :config
-  (setq org-ellipsis " ▾"))
+  (setq org-ellipsis " ▾")
+  (azd/org-font-setup))
+
+  (setq org-agenda-files
+	'("~/org/tasks.org"))
 
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode)
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun azd/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . azd/org-mode-visual-fill))
+ 
